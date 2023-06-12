27ba681a5acdaa9df77d9895d600829675396888
PlatoEnoyer <depp9270@gmail.com>
2023-06-12 03:48:46 +0200

    Начало построения вьюх

--- ml_model/test.py
+++ ml_model/test.py
@@ -2,7 +2,5 @@ import joblib
 import pandas as pd
 
 rf = joblib.load('joblib_model.pkl')
-d = {'region': 39, 'level': 3, 'levels': 5, 'rooms': 2, 'area': 50.0, 'kitchen_area': 10.0}
-print(rf.predict(pd.DataFrame(data=d, index=[0])))
 
 
